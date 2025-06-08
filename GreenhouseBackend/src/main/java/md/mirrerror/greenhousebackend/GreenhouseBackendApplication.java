40e8530b3fd0856d952be5e39fba26bf16d061a5
mirrerror <cayote20@mail.ru>
2025-06-08 19:47:14 +0300

    fix startup issues

--- GreenhouseBackend/src/main/java/md/mirrerror/greenhousebackend/GreenhouseBackendApplication.java
+++ GreenhouseBackend/src/main/java/md/mirrerror/greenhousebackend/GreenhouseBackendApplication.java
@@ -1,5 +1,7 @@
 package md.mirrerror.greenhousebackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GreenhouseBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        for (DotenvEntry entry : dotenv.entries()) System.setProperty(entry.getKey(), entry.getValue());
         SpringApplication.run(GreenhouseBackendApplication.class, args);
     }
 
